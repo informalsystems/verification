d730328bdbe54df972eeab945ee562452e0c70ec
Igor Konnov <igor@interchain.io>
2019-11-19 22:34:29 +0100

    a bit of scripting to run tlc

--- spec/light-client/check-all.sh
+++ spec/light-client/check-all.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-wget -nc https://github.com/tlaplus/tlaplus/releases/download/v1.6.0/tla2tools.jar
-
-java -cp ./tla2tools.jar tlc2.TLC -config MC1.cfg MC1.tla >FinishPositive.out
-java -cp ./tla2tools.jar tlc2.TLC -config MC2.cfg MC2.tla >FinishNegative.out
-java -cp ./tla2tools.jar tlc2.TLC -config MC3.cfg MC3.tla >Correctness.out
-java -cp ./tla2tools.jar tlc2.TLC -config MC4.cfg MC4.tla >Precision.out
+for file in MC1 MC2 MC3 MC4; do
+  ../../auto/tlc-hauself $file.cfg $file.tla
+done
 
