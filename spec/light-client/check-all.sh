b0e72e79887270f422d54d527b7b871fba3a134a
Igor Konnov <igor@interchain.io>
2019-11-19 23:01:06 +0100

    configs for checking all the required properties

--- spec/light-client/check-all.sh
+++ spec/light-client/check-all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for file in MC1 MC2 MC3 MC4; do
-  ../../auto/tlc-hauself $file.cfg $file.tla
+for INV in PrecisionInv NoDupsInv StoredHeadersAreSoundOrNonTrusted; do
+    sed "s/<invariant>/$INV/" <MC1.cfg >MC1-${INV}.cfg
+done
+
+for file in MC1-PrecisionInv MC1-NoDupsInv MC1-StoredHeadersAreSoundOrNonTrusted; do
+  ../../auto/tlc-hauself $file.cfg MC1.tla
 done
 
