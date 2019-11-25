453762d27d0276233a0defa7ac2a75e4046636a9
Igor Konnov <igor@interchain.io>
2019-11-25 13:37:16 +0100

    the script to produce traces that could be used for testing

--- spec/light-client/make-traces.sh
+++ spec/light-client/make-traces.sh
@@ -4,6 +4,7 @@
 
 for INV in NeverFinishPositive NeverFinishNegative; do
     sed "s/<invariant>/$INV/" <MC1.cfg >MC1-${INV}.cfg
-    timeout 60m ../../auto/tlc-hauself MC1-$INV.cfg MC4nodes3blocks.tla -continue
+    timeout 60m ../../auto/tlc-hauself MC1-$INV.cfg MC4nodes3blocks.tla \
+        -depth 20 -continue # simulation up to depth 20, produce all examples
 done
 
